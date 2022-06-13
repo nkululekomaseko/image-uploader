9e0a8674d9e11805e1e5f9e894000af8e8192c1b
Nkululeko <nkululekob.maseko@gmail.com>
2022-06-13 20:15:31 +0200

    Added image link box

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import Upload from "./components/Upload/Upload";
+import ImageLink from "./components/ImageLink/ImageLink";
 import "./app.css";
+import iLink from "./pexels-blaque-x-863963.jpg";
 
 function App() {
-  return <Upload />;
+  // return <Upload />;
+  return <ImageLink imageLink={iLink} />;
 }
 
 export default App;
