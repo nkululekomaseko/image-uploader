0c3655edcd8b4a9c120770bd68d70f5afaa0dfcd
Nkululeko <nkululekob.maseko@gmail.com>
2022-06-27 21:12:19 +0200

    Used cloudinary to upload images

--- app.ts
+++ app.ts
@@ -2,6 +2,19 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import multer from "multer";
 import path from "path";
+import { v2 as cloudinary } from "cloudinary";
+import dotenv from "dotenv";
+import short from "short-uuid";
+
+dotenv.config();
+
+const { CLOUD_NAME, API_KEY, API_SECRET } = process.env;
+
+cloudinary.config({
+  cloud_name: CLOUD_NAME,
+  api_key: API_KEY,
+  api_secret: API_SECRET,
+});
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -17,7 +30,6 @@ const storage = multer.diskStorage({
     callback(null, file.originalname);
   },
 });
-// console.log(`Request: ${}`);
 const upload = multer({ storage: storage });
 
 // app.use("/api/image", express.static(path.join(__dirname, "images")));
@@ -33,16 +45,48 @@ app.get("/api/image/:filename", (request: Request, response: Response) => {
   }
 });
 
+// app.post(
+//   "/api/image",
+//   upload.single("file"),
+//   (request: Request, response: Response) => {
+//     try {
+//       if (request.file)
+//         response.status(200).send({
+//           imageLink: path.join("api", "image", request.file.originalname),
+//         });
+//       else {
+//         response.status(400);
+//       }
+//     } catch (error) {
+//       response.status(500).send(error);
+//     }
+//   }
+// );
+
+let cloudinary_upload_stream = cloudinary.uploader.upload_stream(
+  (error, image) => {}
+);
+
 app.post(
   "/api/image",
   upload.single("file"),
   (request: Request, response: Response) => {
     try {
-      if (request.file)
+      if (request.file) {
+        console.log(`Generated: ${short.generate()}`);
+        cloudinary.uploader.upload(
+          path.join(__dirname, "images", request.file.originalname),
+          { folder: "unsplash" },
+          (error, image) => {
+            if (!!error)
+              console.log(`Error: ${JSON.stringify(error, null, 2)}`);
+            console.log(`Image: ${JSON.stringify(image, null, 2)}`);
+          }
+        );
         response.status(200).send({
           imageLink: path.join("api", "image", request.file.originalname),
         });
-      else {
+      } else {
         response.status(400);
       }
     } catch (error) {
