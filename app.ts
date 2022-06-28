3034170921c1a8c879a02df5fd8c31a497bae094
Nkululeko <nkululekob.maseko@gmail.com>
2022-06-28 23:37:37 +0200

    Testing image buffers

--- app.ts
+++ app.ts
@@ -3,6 +3,9 @@ import cors from "cors";
 import multer from "multer";
 import path from "path";
 import { storage } from "./cloudinary-storage/storage";
+import client from "https";
+import { v2 as cloudinary } from "cloudinary";
+import { createWriteStream } from "fs";
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -13,16 +16,61 @@ const upload = multer({ storage });
 
 // app.use("/api/image", express.static(path.join(__dirname, "images")));
 
-app.use(express.static(path.join(__dirname, "..", "ui/build")));
+const urlToBuffer = async (url: string): Promise<Buffer> => {
+  return new Promise((resolve, reject) => {
+    const data: Uint8Array[] = [];
 
-app.get("/api/image/:filename", (request: Request, response: Response) => {
-  try {
-    let filename: string = request.params.filename;
-    response.status(200).sendFile(path.join(__dirname, "images", filename));
-  } catch (error) {
-    response.status(500).send(error);
+    client.get(url, (response) => {
+      response
+        .on("data", (chunk: Uint8Array) => {
+          data.push(chunk);
+        })
+        .on("end", () => {
+          resolve(Buffer.concat(data));
+        })
+        .on("error", (error) => {
+          reject(error);
+        });
+    });
+  });
+};
+
+app.use(express.static(path.join(__dirname, "..", "ui/build")));
+const file = createWriteStream("test.jpg");
+
+app.get(
+  "/api/image/:filename",
+  async (request: Request, response: Response) => {
+    try {
+      // const imageBuffer: Buffer = await urlToBuffer(
+      //   "https://res.cloudinary.com/nkululeko/image/upload/v1656358320/unsplash/bhivfj6styznkftgffgy.jpg"
+      // );
+
+      const imageRequest = client.get(
+        "https://res.cloudinary.com/nkululeko/image/upload/v1656358320/unsplash/bhivfj6styznkftgffgy.jpg",
+        (resp) => {
+          resp.pipe(file);
+        }
+      );
+      file.close();
+
+      console.log(`imageRequest: ${JSON.stringify(file)}`);
+
+      // console.log(
+      //   `Repsonse from cloudinary: ${JSON.stringify(
+      //     imageBuffer.toString("base64"),
+      //     null,
+      //     2
+      //   )}`
+      // );
+      let filename: string = request.params.filename;
+      response.write(file);
+      response.end();
+    } catch (error) {
+      response.status(500).send(error);
+    }
   }
-});
+);
 
 // app.post(
 //   "/api/image",
